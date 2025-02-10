a49920b2553d6c148e130358303ff31b27c36bf9
toanf2106 <phamtoan17tb@gmail.com>
2025-02-10 12:04:12 +0700

    ok2

--- src/main/java/com/example/student/controller/HsController.java
+++ src/main/java/com/example/student/controller/HsController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -15,6 +16,11 @@ public class HsController {
   @Autowired
   hocSinhRepo hocSinhRepo;
 
+@GetMapping("/oki")
+public String oki(Model model) {
+  model.addAttribute("hocSinh", new HocSinh());
+  return "oki";
+}
   @GetMapping("/hien_thi")
   public String hienThi(Model model, @ModelAttribute HocSinh hocSinh) {
     model.addAttribute("list", hocSinhRepo.findAll());
@@ -25,6 +31,21 @@ public class HsController {
   public String add(@ModelAttribute("hocSinh") HocSinh hocSinh) {
     hocSinhRepo.save(hocSinh);
     return "redirect:/hien_thi";
-  };
+  }
 
+
+  @GetMapping("/hoc-sinh/delete/{id}")
+  public String delete(@PathVariable("id") Integer id) {
+    hocSinhRepo.deleteById(id);
+    return "redirect:/hien_thi";
+
+  }
+
+  @GetMapping("/hoc-sinh/detail/{id}")
+  public String detail(@PathVariable("id") Integer id, Model model) {
+    HocSinh hocSinh = hocSinhRepo.findById(id).get();
+    model.addAttribute("hocSinh", hocSinh);
+    return "/hoc-sinh/detail.html";
+
+  }
 }
